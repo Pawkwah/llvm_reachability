eb738edcdda039c9237e1149463a8cdcd3b31524
Pawkwah <pjharris1998@gmail.com>
2021-04-21 19:43:55 +0000

    initial commit

---
+++ project3/CMakeFiles/project3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bin/project3"
+  "../bin/project3.pdb"
+  "CMakeFiles/project3.dir/project3.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
